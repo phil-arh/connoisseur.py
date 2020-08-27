8a1b1e6bc4235f4c97aef5fbe0f87d6a73027b4c
Phil Howe <phil.a.r.howe@gmail.com>
2020-08-27 19:21:38 +0100

    Update paths in test to match new location of connoisseur.py

--- test/test_cli.py
+++ test/test_cli.py
@@ -34,7 +34,7 @@ def test_tidy():
     subprocess.run(
         [
             "python",
-            "connoisseur.py",
+            "connoisseur/connoisseur.py",
             "tidy",
             "-y",
             "-v",
@@ -51,7 +51,7 @@ def test_copy():
     subprocess.run(
         [
             "python",
-            "connoisseur.py",
+            "connoisseur/connoisseur.py",
             "copy",
             "-y",
             "-v",
@@ -71,7 +71,7 @@ def test_dry_run_tidy():
     subprocess.run(
         [
             "python",
-            "connoisseur.py",
+            "connoisseur/connoisseur.py",
             "tidy",
             "--dry-run",
             "./test/spec_files/test_tidy_reject_spec",
@@ -81,7 +81,7 @@ def test_dry_run_tidy():
     subprocess.run(
         [
             "python",
-            "connoisseur.py",
+            "connoisseur/connoisseur.py",
             "tidy",
             "-d",
             "./test/spec_files/test_tidy_reject_spec",
@@ -100,7 +100,7 @@ def test_dry_run_copy():
     subprocess.run(
         [
             "python",
-            "connoisseur.py",
+            "connoisseur/connoisseur.py",
             "copy",
             "--dry-run",
             "./test/spec_files/test_tidy_reject_spec",
@@ -111,7 +111,7 @@ def test_dry_run_copy():
     subprocess.run(
         [
             "python",
-            "connoisseur.py",
+            "connoisseur/connoisseur.py",
             "copy",
             "-d",
             "./test/spec_files/test_tidy_reject_spec",
